1a36e9ae9943519ab664a4d8246a36d1726a5f80
NagajyothiChukka <nagajyotichukka@gmail.com>
2024-12-08 19:07:43 +0530

    feat/login

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-let userNames=["Star", "Moon", "Galaxy"];
-function signup(str){
-    let filteredData = userNames.filter((el, i) => el==str);
-    if(filteredData.length == 0){
-        userNames.push(str);
-        console.log("signup Sucessful....");
-    }else {
-        console.log("Already Registered....");
-    }
+let userNames = ["Star", "Moon", "Galaxy"];
+function signup(str) {
+  let filteredData = userNames.filter((el, i) => el == str);
+  if (filteredData.length == 0) {
+    userNames.push(str);
+    console.log("signup Sucessful....");
+  } else {
+    console.log("Already Registered....");
+  }
 }
 
 signup("Star");
+
+function login(str, password) {
+  let filteredData = userNames.filter((el, i) => el == str);
+  if (filteredData.length == 0) {
+    console.log("Use Not Found...Please Signup");
+  } else {
+    if (password == "Emp@123") {
+      console.log("Login Sucessfull....");
+    } else {
+      console.log("Wrong Password");
+    }
+  }
+}
+login("Star", "emp@123");
